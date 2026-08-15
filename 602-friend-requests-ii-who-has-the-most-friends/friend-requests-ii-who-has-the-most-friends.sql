85bb90bb6c9b89f33698e7e8bd80341b2db6adf3
Sudhish Rithvik <sudhishrithvik003@gmail.com>
2026-08-15 10:19:25 +0530

    Time: 727 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,17 @@
+WITH AllFriends AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+)
+
+SELECT
+    id,
+    COUNT(*) AS num
+FROM AllFriends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
