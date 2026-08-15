4fc56cb08d0cc897190935de76fb97c2077b2dd5
Sudhish Rithvik <sudhishrithvik003@gmail.com>
2026-08-15 10:17:04 +0530

    Time: 943 ms (45.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+SELECT person_name
+FROM (
+    SELECT
+        person_name,
+        turn,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) AS q
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
